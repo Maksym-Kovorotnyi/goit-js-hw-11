1ad53914ef5b5fc718b0b9828260e8613cca4c11
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-01-31 12:22:15 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import  fetchImg  from './fitchImg';
 import onImgMurkup from './murkup';
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more')
 form.addEventListener('submit', onFormSumbited);
+loadMoreBtn.classList.add('is-hiden')
 let page = 1
 function onFormSumbited(e) {
     e.preventDefault();
@@ -23,9 +26,13 @@ function onFormSumbited(e) {
             return
         }
         if (inputValue) {
-            console.log(images.hits.length)
             clearMurkup(e)
             gallery.insertAdjacentHTML('beforeend', onImgMurkup(images.hits)) 
+            
+            const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+            })
+            loadMoreBtn.classList.remove('is-hiden')
         }       
 loadMoreBtn.addEventListener('click', onLoadMore)
     function onLoadMore(e) {
@@ -33,7 +40,8 @@ loadMoreBtn.addEventListener('click', onLoadMore)
         fetchImg(inputValue, page).then(images => {
             gallery.insertAdjacentHTML('beforeend', onImgMurkup(images.hits))
              if (images.hits.length < 40) {
-            Notiflix.Notify.info('We are sorry, but you have reached the end of search results') 
+                 Notiflix.Notify.info('We are sorry, but you have reached the end of search results') 
+                 loadMoreBtn.classList.add('is-hiden')
             return
        }
         }) 
--- src/murkup.js
+++ src/murkup.js
@@ -2,23 +2,26 @@ export default function onImgMurkup(obj) {
     return obj.map(image => {
         return `
          <div class="photo-card">
-  <img width = 200px src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <a class = 'gallery__link' href = '${image.largeImageURL}'><img class = 'galery__img' src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
-      <b>Likes: ${image.likes}</b>
+      <b>Likes ${image.likes}</b>
     </p>
     <p class="info-item">
-      <b>Views: ${image.views}</b>
+      <b>Views ${image.views}</b>
     </p>
     <p class="info-item">
-      <b>Comments: ${image.comments}</b>
+      <b>Comments ${image.comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads: ${image.downloads}</b>
+      <b>Downloads ${image.downloads}</b>
     </p>
+    </div>
+    </div>
 `    
     }).join('');       
 }
+
  
 
 
